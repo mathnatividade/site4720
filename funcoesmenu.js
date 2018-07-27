0776b998ef5f6e15e2839c385f473ef43291e934
Matheus Natividade <mathnatividade@gmail.com>
2018-07-27 00:05:15 -0300

    Adicionando os outros arquivos

---
+++ funcoesmenu.js
@@ -0,0 +1,29 @@
+function toggle(clubesli) {
+    var el = document.getElementById(clubesli);
+    if ( el.style.display != 'none' ) {
+        el.style.display = 'none';
+    }
+    else {
+        el.style.display = '';
+    }
+}
+
+function toggle(associadosli) {
+    var el = document.getElementById(associadosli);
+    if ( el.style.display != 'none' ) {
+        el.style.display = 'none';
+    }
+    else {
+        el.style.display = '';
+    }
+}
+
+function toggle(usersli) {
+    var el = document.getElementById(usersli);
+    if ( el.style.display != 'none' ) {
+        el.style.display = 'none';
+    }
+    else {
+        el.style.display = '';
+    }
+}
